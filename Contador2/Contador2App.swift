7ff04c04ba12ac5842234436685e8a489514058b
GabrielaAlcaraz <gabriela.alcaraz@cetys.mx>
2025-01-22 20:57:13 -0800

    Initial Commit

---
+++ Contador2/Contador2App.swift
@@ -0,0 +1,17 @@
+//
+//  Contador2App.swift
+//  Contador2
+//
+//  Created by Gabriela Sanchez on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Contador2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Contador2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Contador2
+//
+//  Created by Gabriela Sanchez on 22/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
